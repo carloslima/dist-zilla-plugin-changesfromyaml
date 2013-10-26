4cfb0e314f24311848a39d839700cef2459f8f9d
Carlos Lima <carlos@cpan.org>
2013-10-26 19:58:33 +0800

    Build results of 767b379 (on master)

---
+++ lib/Dist/Zilla/Plugin/ChangesFromYaml.pm
@@ -0,0 +1,24 @@
+package Dist::Zilla::Plugin::ChangesFromYaml;
+# ABSTRACT: convert Changes from YAML to CPAN::Changes::Spec format
+our $VERSION = 0.01;
+
+use Moose;
+with( 'Dist::Zilla::Role::FileMunger' );
+use namespace::autoclean;
+use Dist::Zilla::Plugin::ChangesFromYaml::Convert qw(convert);
+
+sub munge_file {
+    my ($self, $file) = @_;
+
+    return unless $file->name eq 'Changes';
+
+    $file->content( convert( $file->content ) );
+    $self->log_debug(
+        [
+            'Converte Changes from YAML to CPAN::Changes::Spec', $file->name
+        ]
+    );
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
